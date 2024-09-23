4670e98bec2ee75083c483300c0798d2fc82c08e
Leonardo Lobas <leonardolobas2703@gmail.com>
2024-09-23 17:07:50 -0300

    Exercicio inicial

--- exemplo.py
+++ exemplo.py
@@ -1,12 +1,28 @@
-rm = input("Insira seu RM")
-idade = input("Insira sua idade")
-if int(idade) >= 18:
-    print("Sua participação foi autorizada, aluno de RM {}!".format(rm))
-    print("Mais informações serão enviadas para seu e-mail cadastrado!")
-else:
-    autorizado = input("Você possui autorização dos responsáveis? S-SIM ou N-NÃO")
-    if autorizado == 'S':
-        print("Sua participação foi autorizada, aluno de RM {}!".format(rm))
-        print("Mais informações serão enviadas para o email dos responsáveis!")
-    else:
-        print("Sua participação não foi autorizada por causa da sua idade")
+batimentos = float(input('Digite seu batimento:'))
+idade = int(input('Digite sua idade:'))
+
+if idade <= 2 and 120 <= batimentos <= 140 :
+    print('Seu batimento esta dentro da faixa adequada')
+elif 8 <= idade <= 18 and 80 <= batimentos <= 100:
+    print ('Seu batimento esta dentro da faixa adequada')
+elif idade >= 18 and 70 <= batimentos <= 80: 
+    print('Seu batimento esta dentro da faixa adequada')
+elif idade > 60 and 50 <= batimentos <= 60:
+    print('Seu batimento esta dentro da faixa adequada')
+else: 
+    print('Vá para o hospital')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
